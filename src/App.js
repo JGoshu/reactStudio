57cc90f7604db336fc55d33aa6da66f40ee51e73
Jonathan Goshu <jonathan_goshu@brown.edu>
2022-11-14 17:40:14 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem.js"
+
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -8,20 +10,44 @@ bakeryData.forEach((item) => {
 });
 /* ############################################################## */
 
+
+
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [total, setTotal] = useState(0)
+  const cartItems = []
+  
+  // function updateCart(props){
+  //   () => setTotal(total + item.price)
+  //   cartItems.push(promps.name)
+  // }
 
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        // <p>Bakery Item {index}</p>
+        <div>
+       <BakeryItem props={item}/>  
+       <button onClick={() => (setTotal(total + item.price), cartItems.push(item.name))}>Add to Cart</button>
+      
+       </div>
+
       ))}
 
       <div>
         <h2>Cart</h2>
+        {cartItems.map((item) => (
+          <div>
+          <h3>{item}</h3>
+          </div>
+        ))
+        
+        
+        }
+        <h1>${total}</h1>
         {/* TODO: render a list of items in the cart */}
       </div>
     </div>
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,11 @@
 // TODO: create a component that displays a single bakery item
+export default function BakeryItem({props}){
+    return(
+        <>
+        <h3>{props.name}</h3>
+        <h4>{props.price}</h4>
+        <p>{props.description}</p>
+        <img src={props.image} />
+        </>
+    )
+}
